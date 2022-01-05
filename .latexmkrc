0c90f21dbbef021ea7c00bb022a179f10d4d8963
LeixB <abone9999@gmail.com>
2022-01-05 11:42:59 +0100

    Restructure and add flake build

--- .latexmkrc
+++ .latexmkrc
@@ -1,3 +1,3 @@
 $pdf_mode = 4;
-@default_files = ('010-main.tex');
+@default_files = ('000-main.tex');
 $lualatex = "lualatex %O -shell-escape %S";
---
+++ flake.nix
@@ -0,0 +1,82 @@
+{
+  description = "Build LaTeX document with minted";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        latex-packages = with pkgs; [
+          (texlive.combine {
+            inherit (texlive)
+              scheme-medium
+              framed
+              titlesec
+              cleveref
+              multirow
+              wrapfig
+              tabu
+              threeparttable
+              threeparttablex
+              makecell
+              environ
+              biblatex
+              biber
+              fvextra
+              upquote
+              catchfile
+              xstring
+              csquotes
+              minted
+              dejavu
+              comment
+              footmisc
+              xltabular
+              ltablex
+              ;
+          })
+          which
+          python39Packages.pygments
+        ];
+
+        dev-packages = with pkgs; [
+          texlab
+          zathura
+          wmctrl
+        ];
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [ latex-packages dev-packages ];
+        };
+        
+        packages = flake-utils.lib.flattenTree {
+          document = with import nixpkgs { inherit system; }; stdenvNoCC.mkDerivation rec {
+            name = "document.pdf";
+            src = ./.;
+
+            buildInputs = [
+              latex-packages
+            ];
+
+            TEXMFHOME="./cache";
+            TEXMFVAR="./cache/var";
+            SOURCE_DATE_EPOCH = toString self.lastModified;
+
+            buildPhase = ''
+              latexmk
+            '';
+
+            installPhase = ''
+              mkdir -p $out
+              mv *.pdf $out/${name}
+            '';
+          };
+        };
+
+        defaultPackage = self.packages.${system}.document;
+      }
+    );
+}
